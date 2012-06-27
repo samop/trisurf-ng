8854f5104364fc4699d099641b406ce00a95a29a
Samo Penic <samo.penic@gmail.com>
2012-06-27 10:04:20 +0200

    Added test of vesicle radius to co_test

--- src/co_test.c
+++ src/co_test.c
@@ -5,7 +5,7 @@
 #include <math.h>
 
 int main(){
-ts_vesicle *vesicle=initial_distribution_dipyramid(4,60,60,60,0.15);
+ts_vesicle *vesicle=initial_distribution_dipyramid(17,60,60,60,0.15);
 //parsetape(vesicle,&i);
 
 //these four must come from parsetype!
@@ -17,6 +17,7 @@ fprintf(stderr,"xk=%f\n",vesicle->bending_rigidity);
 
 vesicle->sphHarmonics=sph_init(vesicle->vlist, 10);
 int i,j;
+ts_double area;
 for(i=1;i<=vesicle->sphHarmonics->l;i++){
     for(j=1;j<=2*i+1;j++){
     fprintf(stderr,"co(%d,%d)=%e\n",i,j,vesicle->sphHarmonics->co[i][j]);
@@ -27,6 +28,12 @@ vesicle_volume(vesicle);
 fprintf(stderr,"Volume=%e\n",vesicle->volume);
 r0=getR0(vesicle);
 fprintf(stderr,"r0=%e\n",r0);
+area=0;
+for(i=0;i<vesicle->tlist->n;i++){
+	area+=vesicle->tlist->tria[i]->area;
+}
+fprintf(stderr,"area_dipyramid=%e\n",area);
+
 preparationSh(vesicle,r0);
 calculateYlmi(vesicle);
 calculateUlm(vesicle);
