142a67fe82b830e5c7816914afa62445959c87ca
Samo Penic <samo.penic@fe.uni-lj.si>
2013-11-05 15:04:21 +0100

    changes in bondflip call. No need to bondflip all the bonds, but only as many bonds as there are vertices. Also, rnvec seems to be not needed for bondflip, so it is commented out

--- src/timestep.c
+++ src/timestep.c
@@ -30,7 +30,7 @@ ts_bool run_simulation(ts_vesicle *vesicle, ts_uint mcsweeps, ts_uint inititer, 
 ts_bool single_timestep(ts_vesicle *vesicle){
     ts_bool retval;
     ts_double rnvec[3];
-    ts_uint i;
+    ts_uint i, b;
     for(i=0;i<vesicle->vlist->n;i++){
         rnvec[0]=drand48();
         rnvec[1]=drand48();
@@ -39,13 +39,16 @@ ts_bool single_timestep(ts_vesicle *vesicle){
     }
 
 //	ts_int cnt=0;
-    for(i=0;i<vesicle->blist->n;i++){
-        rnvec[0]=drand48();
+    for(i=0;i<vesicle->vlist->n;i++){
+//why is rnvec needed in bondflip?
+/*        rnvec[0]=drand48();
         rnvec[1]=drand48();
         rnvec[2]=drand48();
+*/ 
+	b=rand() % vesicle->blist->n;
         //find a bond and return a pointer to a bond...
         //call single_bondflip_timestep...
-        retval=single_bondflip_timestep(vesicle,vesicle->blist->bond[i],rnvec);
+        retval=single_bondflip_timestep(vesicle,vesicle->blist->bond[b],rnvec);
 //	if(retval==TS_SUCCESS) cnt++;        
     } 
 //	printf("Bondflip success rate in one sweep: %d/%d=%e\n", cnt,vesicle->blist->n,(double)cnt/(double)vesicle->blist->n);
