5f4aea824bd6785dfa67f922bbc67a57178ae0d7
Samo Penic <samo.penic@gmail.com>
2016-05-15 17:52:41 +0200

    Added better help and raw statistics output

--- python/trisurf/tsmgr.py
+++ python/trisurf/tsmgr.py
@@ -1,17 +1,33 @@
 import sys, getopt
 import tabulate
 
+
+def printHelp():
+	print('Python module tsmgr accept following switches:\n')
+	print('tsmgr [-n process number] [-R] [-h] [-r] [-s] [-c comment text] [-a comment text]\n')
+	print('[-n process number]: number of process for which -s -r -c or -a switch apply. Should be placed before any other switch');
+	print('[-R]               : raw output for -s switch');
+	print('[-r]               : run process');
+	print('[-s]               : process status');
+	print('[-c comment text]  : write new comment for process');
+	print('[-a comment text]  : append additional comment for process');
+	print('[-h]               : print help');
+
+
 def start(Runs):
 	argv=sys.argv[1:]
 	processno=0
+	raw=False
 	try:
-		opts, args = getopt.getopt(argv,"a:n:hrsc:")
+		opts, args = getopt.getopt(argv,"Ra:n:hrsc:")
 	except getopt.GetoptError:
-		print('tsmgr [-n process number] [-h] [-r] [-s] [-c comment text] [-a comment text]')
+		printHelp()
 		sys.exit(2)
 	for opt, arg in opts:
-		if opt == '-h':
-			print ('tsmgr [-n process number] [-h] [-r] [-s] [-c comment text] [-a comment text]')
+		if opt == '-R':
+			raw=True
+		elif opt == '-h':
+			printHelp()
 			sys.exit()
 		elif opt == '-r':
 			if processno:
@@ -32,9 +48,11 @@ def start(Runs):
 				line.insert(0,i)
 				report.append(line)
 				i=i+1
-				#print(reportstr)
-			print ("\n\nTrisurf running processes report\n")
-			print (tabulate.tabulate(report,headers=["Run no.", "Run start time", "ETA", "Status", "PID", "Path", "Comment"], tablefmt='fancy_grid'))
+			if(raw):
+				print(report)
+			else:
+				print ("\n\nTrisurf running processes report\n")
+				print (tabulate.tabulate(report,headers=["Run no.", "Run start time", "ETA", "Status", "PID", "Path", "Comment"], tablefmt='fancy_grid'))
 		elif opt == '-n':
 			processno=int(arg)
 			if processno<1 or processno>len(Runs) :
@@ -50,7 +68,7 @@ def start(Runs):
 
 			
 		else:
-			print('tsmgr [-n process number] [-h] [-r] [-s] [-c comment text] [-a comment text]')
+			printHelp()
 			sys.exit(2)
 
 
